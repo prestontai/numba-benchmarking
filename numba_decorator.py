4ce76c95d9037e6ac8dcb3ca780e9f7cd0e0394b
brandontom96 <brandontom96@gmail.com>
2020-01-21 12:52:33 -0800

    Add files via upload
    made it so that import is before import __future__ and shebangs

--- numba_decorator.py
+++ numba_decorator.py
@@ -2,26 +2,55 @@ import sys
 import re
 
 
-def run(input_path, output_path):
+def run(inputfile, outputfile):
     regexfunc = '^def [\w]+\([\w\,]*\):$'
     decorator = '@jit\n'
+    regexshebang = '^#![/\w+]+/*$'
     prog = re.compile(regexfunc)
+    prog2 = re.compile(regexshebang)
+    shebangline = -1
+    futureline = -1
     
+    i = open(inputfile, 'r')
+    o = open(outputfile, 'w')
 
-    input_file = open(input_path, 'r')
-    optimized_file = open(output_path, 'w+')
+    for line in enumerate(i):
+        if prog2.match(line[1]):
+            shebangline = line[0]
 
-    optimized_file.write('from numba import jit\n')
+        elif line[1] == 'import __future__\n':
+            futureline = line[0]
 
-    for line in input_file:
-        if prog.match(line):
-            optimized_file.write(decorator)
-            optimized_file.write(line)
-        else:
-            optimized_file.write(line)
+    print(shebangline, futureline)
 
-    input_file.close()
-    optimized_file.close()
+    i.seek(0)
+
+    if shebangline == -1 and futureline == -1:
+        o.write('from numba import jit\n')
+        
+        for line in i:
+            if prog.match(line):
+                o.write(decorator)
+                o.write(line)
+            else:
+                o.write(line)
+    else:
+        beginning = max(shebangline, futureline)
+        
+        for line in enumerate(i):
+            if line[0] == beginning:
+                o.write(line[1])
+                o.write('from numba import jit\n')
+            elif prog.match(line[1]):
+                o.write(decorator)
+                o.write(line[1])
+            else:
+                o.write(line[1])
+                
+            
+
+    i.close()
+    o.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
