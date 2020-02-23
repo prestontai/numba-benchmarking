29d07e451a50db6dbbc66fd8e3e0f56c0345384d
prestontai <prestonttt@yahoo.com>
2020-02-22 21:17:28 -0800

    plotter update

--- plotter.py
+++ plotter.py
@@ -1,14 +1,23 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+from pymongo import MongoClient
 import plotly.graph_objects as go
 import os
 import json
+import numpy as np
+from numba import jit
 
-STATS = 'stats.txt'
+STATS = 'new_stats.txt'
 
-names = ['python-base', 'numba-base', 'python-intel', 'numba-intel']
-def show_graph(size, time_list):
+#names = ['python-base', 'numba-base', 'python-intel', 'numba-intel']
+client = MongoClient('mongodb+srv://cluster0-at3ay.gcp.mongodb.net/test',
+                      username='preston',
+                      password='BnzA7WVQTCVeCdn3',
+                      authMechanism='SCRAM-SHA-1')
+db = client.mydb
+
+names = ['python', 'numba']
+def show_graph(size, time_list, runs):
     fig = go.Figure()
-
     for index, time in enumerate(time_list):
         fig.add_trace(go.Scatter(
             x=size,
@@ -16,22 +25,33 @@ def show_graph(size, time_list):
             name = names[index],
             connectgaps=True, # override default to connect the gaps
         ))
-    fig.update_layout(xaxis_type = 'log')
+    fig.update_layout(xaxis_type = 'log',
+                      title = 'Runs: ' + str(runs),
+    )
     fig.show()
 
-def parse_stats(path):
-    with open(path) as stats:
-        s = stats.readlines()
-    size = json.loads(s[0])
-    time_list = []
-    for s in s[1:]:
-        time_list.append(json.loads(s))
-
-    return size, time_list
+def parse_stats():
+    mongo = db.stats.find_one({'name': 'tsc2.py'})
+    size_list = mongo['sizes']
+    runs = mongo['runs']
+    proc_python = mongo['python']
+    proc_numba = mongo['numba']
+    '''
+    for line, t in enumerate(time_list):
+        if line%2 == 0:
+            proc_python.append(t)
+        else:
+            proc_numba.append(t)
+    '''
+    time_list = [
+                 np.mean(proc_python, axis = 0),
+                 np.mean(proc_numba, axis = 0)
+                ]       
+    return size_list, time_list, runs
 
 if __name__ == '__main__':
     clean_args = 'rm ' + STATS
-    os.system(clean_args)
-    os.system('git pull') 
-    size, time_list = parse_stats(STATS)
-    show_graph(size, time_list)
+    #os.system(clean_args)
+    #os.system('git pull') 
+    size, time_list, runs = parse_stats()
+    show_graph(size, time_list, runs)
